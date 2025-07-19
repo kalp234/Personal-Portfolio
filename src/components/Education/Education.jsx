824c65e022aa0ce1d0cc7a0882a5de80c3b50118
kalp234 <130490596+kalp234@users.noreply.github.com>
2025-07-19 19:07:05 +0530

    Career Update

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -12,6 +12,35 @@ const Education = () => {
         </div>
         <div>
           <ol className="relative border-s border-gray-200 dark:border-gray-700 text-white mx-2">
+          <li className="mb-10 ms-6">
+              <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                <svg
+                  className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                </svg>
+              </span>
+              <h3 className="flex items-center mb-1 text-lg font-semibold text-white ">
+                Dhirubhai Ambani University(DAU)
+                <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">
+                  Latest
+                </span>
+              </h3>
+              <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                <p className="mb-2">Gandhinagar,Gujarat.</p>{" "}
+                <p> Academic Year : 2025-2027</p>
+              </time>
+              <div className="mb-4 text-base font-normal  text-gray-200">
+                <p>
+                I have joined Dhirubhai Ambani University, formerly known as DAIICT, for M.Tech in ICT with a specialization in Software Systems.Excited to begin this new academic journey at one of the premier institutes of India!
+                </p>
+
+              </div>
+            </li>
             <li className="mb-10 ms-6">
               <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                 <svg
@@ -26,9 +55,7 @@ const Education = () => {
               </span>
               <h3 className="flex items-center mb-1 text-lg font-semibold text-white ">
                 New LJ Institute of Engineering and Technology
-                <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">
-                  Latest
-                </span>
+               
               </h3>
               <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                 <p className="mb-2">Ahmedabad,Gujarat.</p>{" "}
